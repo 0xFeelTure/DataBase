3cab07a9caaad994ec42e7e0f6b06e5ff66043c3
WANZE0 <92678759+WANZE0@users.noreply.github.com>
2022-10-21 09:42:32 +0700

    Delete vala1a2Api.lua

--- vala1a2Api.lua
+++
@@ -1,5 +0,0 @@
-getgenv().Dataoffset = {
-  ['Fast Attack'] = getupvalues(require(game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework))[2];
-  ["hitboxMagnitude"] = 400;
-  ["Camera"] = require(game.ReplicatedStorage.Util.CameraShaker);
-}
